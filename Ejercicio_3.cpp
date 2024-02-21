11b5055514d0372c9a10260ef3ce9afb8b2076b4
yojuma <andresjcopilsen@gmail.com>
2024-02-20 23:04:34 -0500

    Ejercicio 3 laboratorio 1

--- Ejercicio_3.cpp
+++ Ejercicio_3.cpp
@@ -4,18 +4,29 @@
 using namespace std;
 
 int main(){
-    int dia;
-    char mes;
+    int dia,mes;
+
     cout<<"Ingrese el numero del dia: "<< endl;
     cin>>dia;
 
     cout<<"Ingrese el mes: "<< endl;
     cin>>mes;
 
-    if(isalpha(mes)){
-        cout<<"yes"<< endl;
-    }
+    if(dia==29&&mes==2){
+        cout<<dia<<"/" <<mes<<" es valida en bisiesto"<< endl;
+        }
     else{
-        cout<<"eso no es un mes"<< endl;
-    }
+        if(dia>31){
+           cout<<dia<<"/"<<mes<<" es una fecha invalida"<< endl;
+        }
+        else if(mes>12){
+           cout<<dia<<"/"<<mes<<" es una fecha invalida"<< endl;
+        }
+        else{
+           cout<<dia<<"/"<<mes<<" es una fecha valida"<< endl;
+        }
+
+     }
+
+    return 0;
 }
